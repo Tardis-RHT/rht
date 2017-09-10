26ecc113ccdb56767d138f787fa4ddbe404214d7
digvice <digvice@mail.ru>
2017-09-10 22:59:45 +0300

    Some small improvements

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 $(function(){
 // CHANGE LANGUAGE MENU START
 	$('.header_lang_main').click(function() {
-		$('.header_lang_content').removeClass('invisible');
+	// 	$('.header_lang_content').toggle(function(){
+  //     $(this).removeClass('invisible');
+  //   });
+		$('.header_lang_content').removeClass('invisible'); //Not needed if using Toggle
 		$('#header_shevron').removeClass('zmdi-chevron-down');
 		$('#header_shevron').addClass('zmdi-chevron-up');
 		$('.header_lang').mouseleave(function(){
 			$('.header_lang_content').addClass('invisible');
+			// $('.header_lang_content').css('display', 'none'); //Use it with Toggle
 			$('#header_shevron').addClass('zmdi-chevron-down');
 			$('#header_shevron').removeClass('zmdi-chevron-up');
-
-	  });
+		});
 	});
 // CHANGE LANGUAGE MENU END
 
