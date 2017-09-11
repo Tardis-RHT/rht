2ca11f46763398974c74e0ce617d17d9d0af0d87
Annie Alig <coolahhy@gmail.com>
2017-09-12 01:14:20 +0300

    Section video (#6)
    * Initial commit
    
    * index
    
    * add
    
    * removed standarts
    
    * tttttt "removed standarts"
    
    This reverts commit 345a30b487bfffd5c4b6965f90a34c8ea30170d0.
    
    * Revert "add"
    
    This reverts commit d702cad011a51524d2abf3c6965bc29e1019ea5f.
    
    * Standarts for main page
    
    * added wrapper
    
    * improves for buttons
    
    * improved fonts
    
    * trying to merge
    
    * trying to merge
    
    * merged video section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(function(){
+  $(window).scroll(function() {
+      if($(this).scrollTop() >= 40) {
+          $('.header_wrapper_sub').addClass('stickytop');
+          $('.header_cart').addClass('invisible');
+          $('.header_call-us').removeClass('invisible');
+      }
+      else{
+          $('.header_wrapper_sub').removeClass('stickytop');
+          $('.header_cart').removeClass('invisible');
+          $('.header_call-us').addClass('invisible');
+      }
+  });
+});
+
+$(function(){
 
 // MOBILE MENU
 $('#burger').click(function() {
