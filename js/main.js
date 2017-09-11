f835834224e43313831f953fbffc668ff2e2d752
Anna Vovchenko <coolahhy@gmail.com>
2017-09-12 00:50:19 +0300

    trying to merge

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+  $(window).scroll(function() {
+      if($(this).scrollTop() >= 40) {
+          $('.header_wrapper_sub').addClass('stickytop');
+          $('.header_cart').addClass('invisible');
+          $('.header_call-us').removeClass('invisible');
+      }
+      else{
+          $('.header_wrapper_sub').removeClass('stickytop');
+          $('.header_cart').removeClass('invisible');
+          $('.header_call-us').addClass('invisible');
+      }
+  });
+});
+
