a3ee8a104e9b9d8366b63822aec432c5b6effdc0
frontend <frontend@bionics-iMac.local>
2017-09-14 13:58:46 +0300

    Updated footer from master

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(function(){
+
+// MOBILE MENU
+	$('#burger').click(function() {
+			$('.mobile_menu').toggle();
+			$(this).toggleClass('zmdi-menu').toggleClass('zmdi-close');
+	});
+// MOBILE MENU END
+
+
+// CHANGE LANGUAGE MENU START
+	$('.header_lang_main').click(function() {
+	// 	$('.header_lang_content').toggle(function(){
+  //     $(this).removeClass('invisible');
+  //   });
+		$('.header_lang_content').removeClass('invisible'); //Not needed if using Toggle
+		$('#header_shevron').removeClass('zmdi-chevron-down');
+		$('#header_shevron').addClass('zmdi-chevron-up');
+		$('.header_lang').mouseleave(function(){
+			$('.header_lang_content').addClass('invisible');
+			// $('.header_lang_content').css('display', 'none'); //Use it with Toggle
+			$('#header_shevron').addClass('zmdi-chevron-down');
+			$('#header_shevron').removeClass('zmdi-chevron-up');
+		});
+	});
+// CHANGE LANGUAGE MENU END
+
+// STCIKY HEADER
+	$(window).scroll(function() {
+	  if($(this).scrollTop() >= 40) {
+		  $('.header_wrapper_big').addClass('stickytop');
+		  $('.header_cart_bye-text').addClass('invisible');
+		  $('.header_cart_call').removeClass('invisible');
+		  $('.header_cart_buy').css('padding','0 0 0 20px');
+	  }
+	  else{
+		  $('.header_wrapper_big').removeClass('stickytop');
+		  $('.header_cart_bye-text').removeClass('invisible');
+		  $('.header_cart_call').addClass('invisible');
+	  }
+  });
+// END OF STCIKY HEADER
+ 
+	$("#lightgallery").lightGallery({
+		escKeyescKey: true,
+		mousewheel: false,
+	}); 
+
+
+  $('#lightSlider').lightSlider({
+    gallery: false,
+    item: 1,
+    loop:true,
+    slideMargin: 0,
+	controls: true,
+	adaptiveHeight: false,
+	pager: false
+});
+
+
+
+
+
+
+});
