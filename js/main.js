7e3fbf96731185bcfd382067e044d18d36bab540
Annie Alig <coolahhy@gmail.com>
2017-09-15 02:24:12 +0300

    Section video (with popup)
    * index
    
    * add
    
    * removed standarts
    
    * tttttt "removed standarts"
    
    This reverts commit 345a30b487bfffd5c4b6965f90a34c8ea30170d0.
    
    * Revert "add"
    
    This reverts commit d702cad011a51524d2abf3c6965bc29e1019ea5f.
    
    * Standarts for main page
    
    * added wrapper
    
    * improves for buttons
    
    * improved fonts
    
    * trying to merge
    
    * trying to merge
    
    * merged video section
    
    * fixed slider (section certificates)
    
    * added certificates
    
    * improved carousel
    
    * video popup

--- js/main.js
+++ js/main.js
@@ -125,3 +125,103 @@ $('#lightSlider_certificates').lightSlider({
 // 	sld();
 // 	$(window).resize(sld);
 // });
+
+
+// VIDEO POPUP
+	var toHide = document.getElementsByClassName("toHide");
+	var toShow = document.getElementsByClassName("toShow");
+	var control_btn = document.getElementById("video-play");
+
+	document.cancelFullScreen = document.cancelFullScreen || document.webkitCancelFullScreen ||      document.mozCancelFullScreen;
+	function onFullScreenEnter() {
+	// console.log("Enter fullscreen initiated from iframe");
+
+		for (var i = 0; i < toHide.length; i++){
+			toHide[i].style.display="none";
+		};
+
+		for (var i = 0; i < toShow.length; i++){
+			toShow[i].style.display="block";
+		};
+	};
+	function onFullScreenExit() {
+	// console.log("Exit fullscreen initiated from iframe");
+
+		for (var i = 0; i < toHide.length; i++){
+    		toHide[i].style.display="block";
+		};
+
+		for (var i = 0; i < toShow.length; i++){
+			toShow[i].style.display="none";
+			toShow[i].style.visibility="hidden";
+		};	
+	};
+	// Note: FF nightly needs about:config full-screen-api.enabled set to true.
+	function enterFullscreen(id) {
+	onFullScreenEnter(id);
+	var el =  document.getElementById(id);
+	var onfullscreenchange =  function(e){
+	var fullscreenElement = document.fullscreenElement || document.mozFullscreenElement || document.webkitFullscreenElement;
+	var fullscreenEnabled = document.fullscreenEnabled || document.mozFullscreenEnabled || document.webkitFullscreenEnabled;
+	console.log( 'fullscreenEnabled = ' + fullscreenEnabled, ',  fullscreenElement = ', fullscreenElement, ',  e = ', e);
+	}
+	el.addEventListener("webkitfullscreenchange", onfullscreenchange);
+	el.addEventListener("mozfullscreenchange",     onfullscreenchange);
+	el.addEventListener("fullscreenchange",             onfullscreenchange);
+	if (el.webkitRequestFullScreen) {
+	el.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+	} else {
+	el.mozRequestFullScreen();
+	}
+	document.querySelector('#'+id + ' button').onclick = function(){
+	exitFullscreen(id);
+	}
+	}
+	function exitFullscreen(id) {
+	onFullScreenExit(id);
+	document.cancelFullScreen();
+	document.querySelector('#'+id + ' button').onclick = function(){
+	enterFullscreen(id);
+	}
+	}
+
+	function _fullscreenEnabled() {
+		// FF provides nice flag, maybe others will add support for this later on?
+		if(window['fullScreen'] !== undefined) {
+			console.log('1');
+		  return window.fullScreen;
+		}
+		// 5px height margin, just in case (needed by e.g. IE)
+		var heightMargin = 5;
+		if($.browser.webkit && /Apple Computer/.test(navigator.vendor)) {
+		  // Safari in full screen mode shows the navigation bar, 
+		  // which is 40px  
+		  heightMargin = 42;
+		  console.log('2');
+		}
+		return screen.width == window.innerWidth &&
+			Math.abs(screen.height - window.innerHeight) < heightMargin;
+	  }
+
+	  //VIDEO CONTROLS	
+	
+	function vidplay() {
+		var video = document.getElementById("main_video-video");
+		var button = document.getElementById("video-play");
+		if (video.paused) {
+		   video.play();
+		   button.textContent = "| |";
+		} else {
+		   video.pause();
+		   button.textContent = ">";
+		}
+	 }
+
+	 $('#main_video-video').mousemove(function(){
+		$('#video-play').css('visibility', 'visible');
+		$('#video-play').delay(2000).fadeOut();
+		$('#main_video-video').mousemove(function(){
+			$('#video-play').fadeIn();	
+			$('#video-play').delay(1500).fadeOut();
+		});		
+	});
