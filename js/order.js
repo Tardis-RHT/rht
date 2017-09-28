1676fd0d76251757afd141f7775f102beb51aa4d
frontend <frontend@bionics-iMac.local>
2017-09-28 17:45:10 +0300

    Updated labels for inputs

--- js/order.js
+++ js/order.js
@@ -18,10 +18,34 @@ $(document).ready(function(){
 // CHOOSING DELIVERY METHOD - POST-OFFICE - COURIER
 function deliveryChoose(){
     if($('#order_office').is(':checked')){
-        $('#oder_post-office_number').show('400');
-        $('.oder_deliver-courier').hide('100');       
+        $('#order_post-office_number').show('400');
+        $('#order_post-office_label').show('400');
+        $('.order_deliver-courier').hide('100');       
     }else{
-        $('#oder_post-office_number').hide('100');
-        $('.oder_deliver-courier').show('400');
+        $('#order_post-office_number').hide('100');
+        $('#order_post-office_label').hide('100');        
+        $('.order_deliver-courier').show('400');
     }
 };
+
+
+// ENABLE DESABLE BUTTON
+// $('#Button').prop('disabled', true);
+// $('#Button').prop('disabled', false);
+
+$(document).ready(function(){
+    $(".order_input").focusin(function(){
+      $("label[for='" + $(this).attr("id") + "']").css('opacity','1'); //show label of clicked item 
+    });
+    $(".order_input").focusout(function(){
+      $("label[for='" + $(this).attr("id") + "']").css('opacity','0');
+    });
+  }); 
+// $(document).ready(function(){
+//     $(".show_label").focusin(function(){
+//       $("label[for='" + $(this).attr("id") + "']").hide(); //hide label of clicked item 
+//     });
+//     $(".show_label").focusout(function(){
+//       $("label[for='" + $(this).attr("id") + "']").show();
+//     });
+//   }); 
