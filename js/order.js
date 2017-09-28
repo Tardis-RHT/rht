438349c50929ca2e3061ff6cb601e6c98b0917ca
digvice <digvice@mail.ru>
2017-09-28 19:17:20 +0300

    Order (#75)
    * Started developing the order page
    
    * Some animation done
    
    * some changes
    
    * Finished 1st section of making order
    
    * Some fixes to First section
    
    * Desctop version of order making
    
    * Changed file name
    
    * Added mobile version of the order
    
    * Updated labels for inputs
    
    * Added button enabling
    
    * fixed shopping cart in the  header

--- js/order.js
+++ js/order.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     $('#orderGo').click(function(){
         $('.order_payment-list').show('400');
         $('.order_make_comment').hide('400');
@@ -15,13 +14,52 @@ $(document).ready(function(){
         $('.order_done').css('color', 'rgb(17,24,32)');        
     });
 });
+
+
 // CHOOSING DELIVERY METHOD - POST-OFFICE - COURIER
 function deliveryChoose(){
     if($('#order_office').is(':checked')){
-        $('#oder_post-office_number').show('400');
-        $('.oder_deliver-courier').hide('100');       
+        $('#order_post-office_number').show('400');
+        $('#order_post-office_label').show('400');
+        $('.order_deliver-courier').hide('100');
     }else{
-        $('#oder_post-office_number').hide('100');
-        $('.oder_deliver-courier').show('400');
+        $('#order_post-office_number').hide('100');
+        $('#order_post-office_label').hide('100');
+        $('.order_deliver-courier').show('400');
     }
 };
+
+
+// SHOW HIDE LABELS FOR INPUTS
+$(document).ready(function(){
+    $(".order_input").focusin(function(){
+      $("label[for='" + $(this).attr("id") + "']").css('opacity','1');
+    });
+    $(".order_input").focusout(function(){
+      $("label[for='" + $(this).attr("id") + "']").css('opacity','0');
+    });
+  }); 
+
+
+
+
+// ENABLE DESABLE BUTTON
+// $('#Button').prop('disabled', true);
+// $('#Button').prop('disabled', false);
+
+
+(function() {
+    $('.order_delivery_form > input').keyup(function() {
+        var empty = false;
+        $('.order_delivery_form > input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+        if (empty) {
+            $('#orderGo').attr('disabled', 'disabled');
+        } else {
+            $('#orderGo').removeAttr('disabled');
+        }
+    });
+})()
